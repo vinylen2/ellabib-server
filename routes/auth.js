9de94eb1df16960ac875f90acf2ad05e664afa3b
vinylen2 <gabrielwallen93@gmail.com>
2020-02-19 22:33:59 +0100

    udpates

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,14 @@ const qs = require ('querystring');
 const config = require('../config.json');
 const skolon = require('../config.json').skolon;
 
+const OAuthApi = axios.create({
+  baseURL: 'https://idp.skolon.com/oauth/',
+});
+
+const PartnerApi = axios.create({
+  baseURL: 'https://api.skolon.com/v2/partner/',
+});
+
 async function authAdmin(ctx) {
     const { username, password } = ctx.request.body;
     let auth = false;
@@ -30,15 +38,6 @@ async function logoutAdmin(ctx) {
 
 async function authSkolon(ctx) {
   const code = ctx.params.code;
-
-  let OAuthApi = axios.create({
-    baseURL: 'https://idp.skolon.com/oauth/',
-  });
-
-  let PartnerApi = axios.create({
-    baseURL: 'https://api.skolon.com/v2/partner/',
-  });
-
 
   let body = {
     code,
@@ -69,8 +68,28 @@ async function authSkolon(ctx) {
   } catch (e) { console.log(e) }
 }
 
+async function getUsers(ctx) {
+  console.log('getting users');
+  let body = {
+    grant_type: 'client_credentials',
+  };
+
+  let buf = Buffer.from(`${skolon.client_id}:${skolon.client_secret}`);
+  let encoded = buf.toString('base64');
+
+  let config = {
+    headers: {
+      'Authorization': 'Basic ' + encoded,
+    },
+  };
+
+  const access_token = await OAuthApi.post('access_token', body, config);
+  console.log(access_token);
+};
+
 router.post('/admin', authAdmin);
 router.get('/skolon/:code', authSkolon);
+router.get('/users/', getUsers);
 router.get('/logout', logoutAdmin);
 
 module.exports = router;
